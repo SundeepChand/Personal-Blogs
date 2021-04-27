d1939575a3d00e7800bb2ab714395d6062f9762e
Sundeep Chand <sundeep.chand2001@gmail.com>
2021-04-27 22:31:57 +0530

    Added header image

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,11 +1,28 @@
-import Layout from '../../components/Layout'
+import Layout from "../../components/Layout";
+import Header from "../../components/Header";
 
-export default function() {
+export default function About() {
   return (
     <Layout>
+      <Header imgSrc="/images/hero-about.jpg">
+        Photo by{" "}
+        <a
+          href="https://unsplash.com/@donramxn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          target="_blank"
+        >
+          Ramón Salinero
+        </a>{" "}
+        on{" "}
+        <a
+          href="https://unsplash.com/s/photos/technology?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          target="_blank"
+        >
+          Unsplash
+        </a>
+      </Header>
       <div>
         <h1>About Me</h1>
       </div>
     </Layout>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Layout from '../components/Layout'
-import Header from '../components/Header'
+import Head from "next/head";
+import Layout from "../components/Layout";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -9,26 +9,26 @@ export default function Home() {
         <title>Post</title>
       </Head>
 
-      <Header imgSrc='/images/home-hero.jpg'>
-        Photo by{' '}
+      <Header imgSrc="/images/hero-home.jpg">
+        Photo by{" "}
         <a
-          href="https://unsplash.com/@donramxn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          href="https://unsplash.com/@bertsz?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
           target="_blank"
         >
-          Ramón Salinero
-        </a>{' '}
-        on{' '}
+          bert brrr
+        </a>{" "}
+        on{" "}
         <a
-          href="https://unsplash.com/s/photos/technology?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          href="https://unsplash.com/s/photos/network?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
           target="_blank"
         >
           Unsplash
         </a>
       </Header>
 
-      <div style={{position: 'relative'}}>
+      <div style={{ position: "relative" }}>
         <h1>Home</h1>
       </div>
     </Layout>
-  )
+  );
 }
