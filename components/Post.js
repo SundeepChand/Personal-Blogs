1c677afc66da6071baf3771e6a72dfa8989dc77e
Sundeep Chand <sundeep.chand2001@gmail.com>
2021-04-28 14:08:47 +0530

    Styled posts card

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,36 @@
 import Image from "next/image";
+import { format } from "date-fns";
+import styles from "../styles/components/Post.module.scss";
 
 export default function Post({ post }) {
   return (
-    <div>
-      <Image src={post.imgUrl} width={200} height={300} layout="fixed" />
-      <p>{post.title}</p>
-      <p>{post.author}</p>
-      <p>{post.date}</p>
-      <p>{post.author}</p>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image
+          src={post.imgUrl}
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.title}>{post.title}</div>
+        <div className={styles.meta}>
+          <p className={styles.authorName}>By {post.author}</p>
+          <div>
+            <p className={styles.date}>
+              {format(new Date(post.date), "dd MMM yyyy")}
+            </p>
+          </div>
+        </div>
+        <p className={styles.postDescription}>{slice(post.description)}</p>
+      </div>
     </div>
   );
 }
+
+const slice = (text, length = 130) => {
+  // Function to slice the passed text to a certaine length
+  if (text.length <= length) return text;
+  return text.slice(0, length) + "...";
+};
--- components/PostsLayout.js
+++ components/PostsLayout.js
@@ -1,12 +1,19 @@
 import Post from "./Post";
+import styles from "../styles/components/PostLayout.module.scss";
 
 export default function PostLayout({ posts }) {
   return (
-    <div>
-      <h1>Home</h1>
-      {posts.map((post) => {
-        return <Post key={post.id} post={post} />;
-      })}
+    <div className={styles.container}>
+      {/*
+        <div className={styles.title}>
+          <h1>Home</h1>
+        </div>
+      */}
+      <div className={styles.content}>
+        {posts.map((post) => {
+          return <Post key={post.id} post={post} />;
+        })}
+      </div>
     </div>
   );
 }
