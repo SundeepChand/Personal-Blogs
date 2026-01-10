915519e9d607091d1d1e9264f576bb6b68fa07b8
SundeepChand <sundeep.chand2001@gmail.com>
2026-01-10 18:33:48 +0530

    Add code

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { useEffect } from "react";
 import hljs from "highlight.js/lib/core";
 import cpp from "highlight.js/lib/languages/cpp";
+import java from "highlight.js/lib/languages/java";
+import xml from "highlight.js/lib/languages/xml";
 import "highlight.js/styles/atom-one-dark.css";
 import { remark } from "remark";
 import html from "remark-html";
@@ -16,6 +18,8 @@ import { wavePattern } from "../../assets/patterns/wave";
 import styles from "../../styles/pages/Post.page.module.scss";
 
 hljs.registerLanguage('cpp', cpp);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('xml', xml);
 
 export default function Post({ post }) {
   useEffect(() => {
@@ -105,6 +109,7 @@ export async function getStaticProps({ params }) {
   // Fetch the individual blog post.
   const post = await getPostById(params.id);
   post.content = await markdownToHtml(post.content);
+  console.log(post.content);
   return {
     props: {
       post: post,
