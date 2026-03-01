efaee24d70e39e0ddfbc76a182c56aced2513057
SundeepChand <sundeep.chand2001@gmail.com>
2026-03-02 00:35:32 +0530

    Fix blog post

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import hljs from "highlight.js";
 import cpp from "highlight.js/lib/languages/cpp";
 import plaintext from "highlight.js/lib/languages/plaintext";
 import java from "highlight.js/lib/languages/java";
-import golang from "highlight.js/lib/languages/go";
+import go from "highlight.js/lib/languages/go";
 
 import "../styles/globals.scss";
 import "highlight.js/styles/atom-one-dark.css";
@@ -12,7 +12,7 @@ import "highlight.js/styles/atom-one-dark.css";
 hljs.registerLanguage("cpp", cpp);
 hljs.registerLanguage("plaintext", plaintext);
 hljs.registerLanguage("java", java);
-hljs.registerLanguage("go", golang);
+hljs.registerLanguage("go", go);
 
 function MyApp({ Component, pageProps }) {
   useNProgress();
