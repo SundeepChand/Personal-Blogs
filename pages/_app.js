abc2b7b01f84b8519806c6d1b966e7cee902eaa8
SundeepChand <sundeep.chand2001@gmail.com>
2026-03-01 23:03:21 +0530

    Update tls blog

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,16 @@ import useNProgress from "../hooks/useNProgress";
 import hljs from "highlight.js";
 import cpp from "highlight.js/lib/languages/cpp";
 import plaintext from "highlight.js/lib/languages/plaintext";
+import java from "highlight.js/lib/languages/java";
+import golang from "highlight.js/lib/languages/go";
 
 import "../styles/globals.scss";
 import "highlight.js/styles/atom-one-dark.css";
 
 hljs.registerLanguage("cpp", cpp);
 hljs.registerLanguage("plaintext", plaintext);
+hljs.registerLanguage("java", java);
+hljs.registerLanguage("go", golang);
 
 function MyApp({ Component, pageProps }) {
   useNProgress();
